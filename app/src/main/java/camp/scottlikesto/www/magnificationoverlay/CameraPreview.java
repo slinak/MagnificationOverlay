e6c8a556ce2ef5ebb877091331f9d0fe17852dbb
slinak <scott@linak.org>
2019-05-09 11:56:22 -0400

    cleaned up mainactivity and moved capture stuff into camerapreview

--- app/src/main/java/camp/scottlikesto/www/magnificationoverlay/CameraPreview.java
+++ app/src/main/java/camp/scottlikesto/www/magnificationoverlay/CameraPreview.java
@@ -3,9 +3,14 @@ package camp.scottlikesto.www.magnificationoverlay;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.hardware.Camera;
+import android.os.AsyncTask;
+import android.os.Environment;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import static android.content.ContentValues.TAG;
 
@@ -63,6 +68,57 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
     public void resetCam() {
         cameraDevice.startPreview();
-
     }
+
+    public Camera.ShutterCallback shutterCallback = new Camera.ShutterCallback() {
+        public void onShutter() {
+            //don't care atm
+        }
+    };
+
+    Camera.PictureCallback rawCallback = new Camera.PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+        }
+    };
+
+    Camera.PictureCallback jpegCallback = new Camera.PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            new SaveImageTask().execute(data);
+            resetCam();
+        }
+    };
+
+    public class SaveImageTask extends AsyncTask<byte[], Void, Void> {
+        @Override
+        protected Void doInBackground(byte[]... data) {
+            FileOutputStream outStream = null;
+
+            try {
+                File sdCard = Environment.getExternalStorageDirectory();
+                File dir = new File (sdCard.getAbsolutePath() + "/camtest");
+                dir.mkdirs();
+
+                String fileName = String.format("%d.jpg", System.currentTimeMillis());
+                File outFile = new File(dir, fileName);
+
+                outStream = new FileOutputStream(outFile);
+                outStream.write(data[0]);
+                outStream.flush();
+                outStream.close();
+
+                //refreshGallery(outFile);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+    /*
+    private void refreshGallery(File file) {
+        Intent mediaScanIntent = new Intent( Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        mediaScanIntent.setData(Uri.fromFile(file));
+        sendBroadcast(mediaScanIntent);
+    }
+    */
 }
--- app/src/main/java/camp/scottlikesto/www/magnificationoverlay/MainActivity.java
+++ app/src/main/java/camp/scottlikesto/www/magnificationoverlay/MainActivity.java
@@ -2,25 +2,18 @@ package camp.scottlikesto.www.magnificationoverlay;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.Spinner;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 /*
@@ -52,8 +45,6 @@ public class MainActivity extends AppCompatActivity {
         } else {
             requestPermission();
         }
-
-
 
         camera = getCameraInstance();
 
@@ -89,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         captureImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+                camera.takePicture(cameraPreview.shutterCallback, cameraPreview.rawCallback, cameraPreview.jpegCallback);
             }
         });
     }
@@ -109,62 +100,16 @@ public class MainActivity extends AppCompatActivity {
     public static Camera getCameraInstance() {
         Camera c = null;
         try {
-            c = Camera.open(); // attempt to get a Camera instance
+            c = Camera.open();
         } catch (Exception e) {
-            Log.d(null, "Camera is not available - " + e.getMessage());
+            //camera not available
         }
-        return c; // returns null if camera is unavailable
+        return c;
     }
 
-    private void refreshGallery(File file) {
-        Intent mediaScanIntent = new Intent( Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        mediaScanIntent.setData(Uri.fromFile(file));
-        sendBroadcast(mediaScanIntent);
-    }
-    Camera.ShutterCallback shutterCallback = new Camera.ShutterCallback() {
-        public void onShutter() {
-            //			 Log.d(TAG, "onShutter'd");
-        }
-    };
-    Camera.PictureCallback rawCallback = new Camera.PictureCallback() {
-        public void onPictureTaken(byte[] data, Camera camera) {
-            //			 Log.d(TAG, "onPictureTaken - raw");
-        }
-    };
 
-    Camera.PictureCallback jpegCallback = new Camera.PictureCallback() {
-        public void onPictureTaken(byte[] data, Camera camera) {
-            new SaveImageTask().execute(data);
-            cameraPreview.resetCam();
-            Log.d("YOYOYO - YOYOYO", "onPictureTaken - jpeg");
-        }
-    };
 
-    private class SaveImageTask extends AsyncTask<byte[], Void, Void> {
-        @Override
-        protected Void doInBackground(byte[]... data) {
-            FileOutputStream outStream = null;
 
-            try {
-                File sdCard = Environment.getExternalStorageDirectory();
-                File dir = new File (sdCard.getAbsolutePath() + "/camtest");
-                dir.mkdirs();
 
-                String fileName = String.format("%d.jpg", System.currentTimeMillis());
-                File outFile = new File(dir, fileName);
 
-                outStream = new FileOutputStream(outFile);
-                outStream.write(data[0]);
-                outStream.flush();
-                outStream.close();
-
-                //Log.d("YOYOYO - YOYOYO", "onPictureTaken - wrote bytes: " + data.length + " to " + outFile.getAbsolutePath());
-
-                refreshGallery(outFile);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-    }
 }
